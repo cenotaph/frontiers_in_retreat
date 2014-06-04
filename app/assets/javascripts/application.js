18496e18d7c334bad4c6130458cd5fde7b4464fc
John W. Fail <fail@cenotaph.org>
2014-06-05 00:35:33 +0300

    restored javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,44 +1,18 @@
-
-function toggleDialog(url, id) {
-  var tag = $("<div id=" + id + "></div>");
-  if ($("#" +id).dialog( "isOpen" )===true) {
-
-    $("#" +id).dialog("close");
-    $("#" + id).remove();
-    $('#' + id + "_menu").css('color', 'black');
-  } else {
-    $.ajax({
-      url: url,
-      success: function(data) {
-        tag.html(data).dialog({modal: false, width: '32em', draggable: true,  maxHeight: 500, position: { my: "left top", at: "right+20 top", of: '#main_menu'}
-      }).dialog('open');
-      $('#' + id + "_menu").css('color', 'red');
-        $('.ui-dialog-titlebar button').click(function() {
-          $('#main_menu a').css('color', "#000");          
-        });
-      }
-    });  
-  }
-}
-
-function siteDialogue(url, id) {
-  var tag = $("<div id=" + id + "></div>");
-  if ($("#" +id).dialog( "isOpen" )===true) {
-
-    $("#" +id).dialog("close");
-    $("#" + id).remove();
-    $('#' + id + "_menu").css('color', 'black');
-  } else {
-    $.ajax({
-      url: url,
-      success: function(data) {
-        tag.html(data).dialog({modal: false, width: '32em', draggable: true,  maxHeight: 500, position: { my: "left top", at: "right+20 top", of: '#main_menu'}
-      }).dialog('open');
-      $('#' + id + "_menu").css('color', 'red');
-        $('.ui-dialog-titlebar button').click(function() {
-          $('#main_menu a').css('color', "#000");          
-        });
-      }
-    });  
-  }
-}
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require jquery.ui.core
+//= require jquery.ui.dialog
+//= require turbolinks
+//= require hiap
