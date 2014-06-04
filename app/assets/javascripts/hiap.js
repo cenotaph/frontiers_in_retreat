c2fd39a3eda84b2cc68501c38952db2875caf6c0
John W. Fail <fail@cenotaph.org>
2014-06-05 00:37:38 +0300

    restored javascript

--- app/assets/javascripts/hiap.js
+++ app/assets/javascripts/hiap.js
@@ -1,13 +1,44 @@
-function showUrlInDialog(url){
-   var tag = $("<div></div>");
-   $.ajax({
-     url: url,
-     success: function(data) {
-       tag.html(data).dialog({modal: true, width: '45em', draggable: true, position: { my: "left top", at: "right+20 top", of: '#main_menu'}}).dialog('open');
-       $('.ui-dialog-titlebar button').click(function() {
-         $('#main_menu a').css('color', "#000");          
-       });
-     }
-   });
- }
- 
+
+function toggleDialog(url, id) {
+  var tag = $("<div id=" + id + "></div>");
+  if ($("#" +id).dialog( "isOpen" )===true) {
+
+    $("#" +id).dialog("close");
+    $("#" + id).remove();
+    $('#' + id + "_menu").css('color', 'black');
+  } else {
+    $.ajax({
+      url: url,
+      success: function(data) {
+        tag.html(data).dialog({modal: false, width: '32em', draggable: true,  maxHeight: 500, position: { my: "left top", at: "right+20 top", of: '#main_menu'}
+      }).dialog('open');
+      $('#' + id + "_menu").css('color', 'red');
+        $('.ui-dialog-titlebar button').click(function() {
+          $('#main_menu a').css('color', "#000");          
+        });
+      }
+    });  
+  }
+}
+
+function siteDialog(url, id) {
+  var tag = $("<div id=" + id + "></div>");
+  if ($("#" +id).dialog( "isOpen" )===true) {
+
+    $("#" +id).dialog("close");
+    $("#" + id).remove();
+    $('#' + id + "_menu").css('color', 'black');
+  } else {
+    $.ajax({
+      url: url,
+      success: function(data) {
+        tag.html(data).dialog({modal: false, width: '23em', draggable: true,  maxHeight: 500, position: { my: "left top", at: "left bottom+20", of: '#main_menu'}
+      }).dialog('open');
+      $('#' + id + "_menu").css('color', 'red');
+        $('.ui-dialog-titlebar button').click(function() {
+          $('#main_menu a').css('color', "#000");          
+        });
+      }
+    });  
+  }
+}
