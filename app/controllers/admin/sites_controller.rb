43191c915261bbedd47b3c3b5c85f6cad1eb8f3b
John W. Fail <fail@cenotaph.org>
2015-04-26 17:40:01 +0300

    removing inherited_resources from controller

--- app/controllers/admin/sites_controller.rb
+++ app/controllers/admin/sites_controller.rb
@@ -1,11 +1,54 @@
 class Admin::SitesController < Admin::BaseController
+  respond_to :html
+  handles_sortable_columns
   
   def create
-    create! { admin_sites_path }
+    @site = Site.create(site_params)
+    respond_with @site, location: -> { admin_sites_path}
   end
   
+  def destroy
+    @site = Site.find(params[:id])
+    @site.destroy
+    redirect_to admin_sites_path
+  end
+  
+  def edit
+    @site = Site.find(params[:id])
+    set_meta_tags title: "Edit site"
+  end
+
+
+  def index
+    order = sortable_column_order do |column, direction|
+      case column
+      when "location"
+        "country #{direction}, city #{direction}"
+      when "name"
+        "name #{direction}"
+      when "current"
+        "current #{direction}"
+      when "id"
+        "id #{direction}"
+      else
+        "name ASC"
+      end
+    end
+    @sites = Site.unscoped.all.order(order)
+    set_meta_tags title: 'Sites'
+  end
+  
+  def new
+    @site = Site.new
+    set_meta_tags title: 'New site'
+  end 
+   
   def update
-    update! { admin_sites_path }
+    @site = Site.find(params[:id])
+    if @site.update_attributes(site_params)
+      respond_with @site,  location: -> { admin_sites_path}
+    end
+
   end
   
   protected
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,15 +1,57 @@
 class Admin::UsersController < Admin::BaseController
-  actions :edit, :update, :destroy, :index
+  respond_to :html
+  handles_sortable_columns
   
-  
-  def update
-    update! { admin_users_path }
+  def create
+    @user = User.create(user_params)
+    respond_with @user, location: -> { admin_users_path}
   end
   
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+    set_meta_tags title: "Edit user"
+  end
+
+
+  def index
+    order = sortable_column_order do |column, direction|
+      case column
+      when "email"
+        "email #{direction}"
+      when "username"
+        "username #{direction}"
+      when "id"
+        "id #{direction}"
+      else
+        "id ASC"
+      end
+    end
+    @users = User.unscoped.all.order(order)
+    set_meta_tags title: 'Users'
+  end
+  
+  def new
+    @user = User.new
+    set_meta_tags title: 'New user'
+  end 
+   
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      respond_with @user,  location: -> { admin_users_path}
+    end
+
+  end
   protected
   
   def user_params
-    params.require(:user).permit(:site_id, :approved, :email, :username)
+    params.require(:user).permit(:user_id, :approved, :site_id, :email, :username)
   end
   
 end
