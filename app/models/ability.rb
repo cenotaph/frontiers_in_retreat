94735cc3a2238e4f1953dcd20d0255cea6e1eb30
John W. Fail <john@pixelache.ac>
2015-05-28 13:19:44 -0400

    artist and admin classes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,9 +8,15 @@ class Ability
     elsif user.has_role? :admin
       can :read, :all
       can :modify, User, id: user.id
+      cannot :manage, Site
     elsif user.has_role? :artist
       can :modify, Post, user_id: user.id
       can :create, Post
+      cannot :manage, Site
+      cannot :manage, Page
+      cannot :manage, Event
+      cannot :manage, Artist
+      cannot :manage, Residency
     end
   end
 end
